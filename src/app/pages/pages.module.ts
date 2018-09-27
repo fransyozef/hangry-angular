e99c9945508188f91bd1af2d244a8ee4f65b36d0
Fransjo Leihitu <fransjoleihitu@Fransjos-MacBook-Air.local>
2018-09-27 14:53:26 +0200

    Restaurant add form non reactive

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,12 +5,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantsListComponent } from './restaurants/restaurants-list/restaurants-list.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RestaurantAddComponent } from './restaurants/restaurant-add/restaurant-add.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  declarations: [DashboardComponent, RestaurantsComponent, RestaurantsListComponent, RestaurantComponent]
+  declarations: [DashboardComponent, RestaurantsComponent, RestaurantsListComponent, RestaurantComponent, RestaurantAddComponent]
 })
 export class PagesModule { }
